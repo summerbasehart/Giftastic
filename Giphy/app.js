4bd168a556bc187a0ac8e9f2c167692257af9236
summerbasehart <47164799+summerbasehart@users.noreply.github.com>
2019-03-26 21:56:16 -0700

    Add files via upload

---
+++ Giphy/app.js
@@ -0,0 +1,7 @@
+var Characters = ["Bones", "Spock", "Kirk", "Uhura", "Chekov", "Sulu", "Scotty", "Chapel", "Rand", "Gaila"]
+
+for (var i = 0; i < characters.length; i++) {
+
+}
+
+//giphy api = 6cwDdCnBsu7hiNOyheNhToCQiQTgei7r
