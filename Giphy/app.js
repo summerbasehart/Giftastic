8e41840c923fe1f8d7ad21c649c9dee9e9e0cbe0
unknown <summerbasehart@gmail.com>
2019-06-13 20:13:38 -0700

    updates

--- Giphy/app.js
+++
@@ -1,7 +0,0 @@
-var Characters = ["Bones", "Spock", "Kirk", "Uhura", "Chekov", "Sulu", "Scotty", "Chapel", "Rand", "Gaila"]
-
-for (var i = 0; i < characters.length; i++) {
-
-}
-
-//giphy api = 6cwDdCnBsu7hiNOyheNhToCQiQTgei7r
