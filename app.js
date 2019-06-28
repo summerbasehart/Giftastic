ecc602261ba8a725e4b32bd0378a6da7e328e0a4
unknown <summerbasehart@gmail.com>
2019-06-28 11:22:17 -0700

    expanded

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var Characters = ["Bones", "Spock", "Kirk", "Uhura", "Chekov", "Sulu", "Scotty", "Chapel", "Rand", "Gaila"]
-
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var characters = ["Bones", "Spock", "Kirk", "Uhura", "Chekov", "Sulu", "Scotty", "Chapel", "Rand", "Gaila"]
 for (var i = 0; i < characters.length; i++) {
-
+    var button = $("<button>")
+    button.addClass("gif")
+    button.text(characters[i])
+    $('#buttons').append(button)
 }
+    function getGifs() {
+        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=6cwDdCnBsu7hiNOyheNhToCQiQTgei7r&limit=5");
+        xhr.done(function (data) { 
+            console.log(data)
+        });
+    }
+    getGifs().append('#gifDiv')
+});
+
+
 
 //giphy api = 6cwDdCnBsu7hiNOyheNhToCQiQTgei7r
